9fecc3494150a20d9a9b8ebaea511ca765f05502
Stefano <tefanoboschetti@gmail.com>
2023-10-17 06:06:49 +0000

    Changes to CSS👨‍🎨

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { MdArrowForwardIos } from "react-icons/md"
-import "../../styles/sidebar.css";
+import "../../styles/navigation.css";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar-main">
-			<span className="navbar-user">
+			<div className="navbar-user">
 				<img className="navbar-user-icon"  src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"/>
 				Stefano
 				<MdArrowForwardIos className="navbar-user-arrow"/>
-			</span>
+			</div>
 		</nav>
 	);
 };
--- src/front/js/component/sidebar.js
+++ src/front/js/component/sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { MdSpaceDashboard, MdShoppingBasket, MdReceiptLong, MdPeopleAlt, MdLocalOffer, MdArrowForwardIos, MdStar, MdLogout } from "react-icons/md"
 import { Link } from "react-router-dom";
-import "../../styles/sidebar.css";
+import "../../styles/navigation.css";
 
 
 export const Sidebar = () => {
@@ -9,12 +9,13 @@ export const Sidebar = () => {
 		<div id="sidebar">
             <img src="https://www.nic.do/wp-content/uploads/2016/10/logo-placeholder-3.jpg" className="sidebar-logo"/>
             <button className="shrt-order-btn"><MdStar className="order-heart"/>Nuevo pedido</button>
-            <span className="menu-item"><MdSpaceDashboard className="menu-icon"/>   Dashboard </span>
-            <span className="menu-item"><MdShoppingBasket className="menu-icon"/>   Pedidos <MdArrowForwardIos className="expand-item"/> </span>
-            <span className="menu-item"><MdReceiptLong className="menu-icon"/>   Productos </span>
-            <span className="menu-item"><MdPeopleAlt className="menu-icon"/>   Clientes </span>
-            <span className="menu-item"><MdLocalOffer className="menu-icon"/>   Cupones </span>
-            <span className="menu-logout"><MdLogout className="logout-icon"/>   Cerrar sesión </span>
+            <div className="menu-title">TIENDA</div>
+            <div className="menu-item"><MdSpaceDashboard className="menu-icon"/>   Dashboard </div>
+            <div className="menu-item"><MdShoppingBasket className="menu-icon"/>   Pedidos <MdArrowForwardIos className="expand-item"/> </div>
+            <div className="menu-item"><MdReceiptLong className="menu-icon"/>   Productos </div>
+            <div className="menu-item"><MdPeopleAlt className="menu-icon"/>   Clientes </div>
+            <div className="menu-item"><MdLocalOffer className="menu-icon"/>   Cupones </div>
+            <div className="menu-logout"><MdLogout className="logout-icon"/>   Cerrar sesión </div>
         </div>
 	);
 };
